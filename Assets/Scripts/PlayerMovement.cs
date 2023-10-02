5b32b2fd9ca28f4729ad63f8a5c24d1cb5623831
Kaylon-Riordan <kaylonriordan@gmail.com>
2023-10-02 15:35:23 +0100

    added character with movement script and animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpHeight = 7f;
+    private Rigidbody2D rBody;
+    private Animator mator;
+    private bool grounded;
+    private bool faceRight = true;
+    private void Awake(){
+        rBody = GetComponent<Rigidbody2D>();
+        mator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        rBody.velocity = new Vector2(horizontalInput * speed, rBody.velocity.y);
+
+        if((horizontalInput>0 && !faceRight)||(horizontalInput<0 && faceRight)){
+            Flip();
+        }
+
+        if(Input.GetKey(KeyCode.Space) && grounded){
+            jump();
+        }
+    }
+
+    private void jump(){
+        rBody.velocity = new Vector2(rBody.velocity.x, jumpHeight);
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("Ground"))
+        grounded = true;
+    }
+    private void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        faceRight = !faceRight;
+    }
+}
